74c64cd8f358df4e6e91dc199645bcc627cff8d1
Ellivers <53975650+Ellivers@users.noreply.github.com>
2022-12-02 23:29:41 +0100

    Delete build directory

--- build/assets/js/77a16d3d.c56b0ed9.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkworldtool_docs=self.webpackChunkworldtool_docs||[]).push([[3702],{5745:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
